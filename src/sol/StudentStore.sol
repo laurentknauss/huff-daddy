ccaf386a812402983f23369834f33db6bfdf5669
laurentknauss <laurentknauss@protonmail.com>
2025-03-04 15:18:47 +0100

    modified .huff file

--- src/sol/StudentStore.sol
+++ src/sol/StudentStore.sol
@@ -3,14 +3,14 @@
 pragma solidity 0.8.20;
 
 contract HorseStore {
-    uint256 numberOfHorses; // storage  variable 
+    uint256 numberOfStudents; // storage  variable 
 
-    function updateHorseNumber(uint256 newNumberOfHorses) external {
-        numberOfHorses = newNumberOfHorses;
+    function updateStudentNumber(uint256 newNumberOfStudents) external {
+        numberOfStudents = newNumberOfStudents;
     }
 
-    function readNumberOfHorses() external view returns (uint256) {
-        return numberOfHorses;
+    function readNumberOfStudents() external view returns (uint256) {
+        return numberOfStudents;
     }
 } 
 
